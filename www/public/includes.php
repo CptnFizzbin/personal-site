ef803892e4bfb657cf484196fce3f9afa3a37b8e
Stephen <code@cptnfizzbin.ca>
2014-10-12 18:29:13 -0400

    remove passwords.txt file

--- www/public/includes.php
+++ www/public/includes.php
@@ -1,12 +1,5 @@
 <?php
-  set_include_path( get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] );
-
-  $enableHTTPSRedirect = true;
-  if($enableHTTPSRedirect && !isset($_SERVER['HTTPS']))
-  {
-    header("Location: https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-  }
-
   include(__DIR__."/../config/config.php");
+  set_include_path( get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] );
   include("Layout.php");
 ?>
