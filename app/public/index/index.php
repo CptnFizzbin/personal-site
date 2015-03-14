2760e0256043d84275e365d7e04cb3d271d791d1
Stephen <code@cptnfizzbin.ca>
2015-03-14 19:18:17 -0400

    Update picture

--- app/public/index/index.php
+++ app/public/index/index.php
@@ -7,18 +7,20 @@
 <div class="homepage">
   <section>
     <div class="row">
-      <div class="small-6 small-centered medium-4 large-3 columns">
-        <img src="images/logo.png" id="self-portrait"/>
+      <div class="small-12 medium-4 large-3 columns">
+        <img src="images/Profile-Personal.jpg" id="self-portrait" alt="Stephen A. Wilson"/>
       </div>
-      <div class="small-12 medium-text-center large-6 large-text-left columns">
-        <h1>Stephen A. Wilson</h1>
-      </div>
-      <div class="small-12 medium-text-center large-6 large-text-right columns">
-        <h2 class="subtitle">
-          Student of <br class="hide-for-medium-only"/>
-          Backend Web Development
-        </h2>
-      </div>
+      <div class="small-12 medium-8 large-9 columns">
+        <div class="row">
+          <div class="small-12 small-text-center columns">
+            <h1>Stephen A. Wilson</h1>
+          </div>
+          <div class="small-12 small-text-center columns">
+            <h2 class="subtitle">
+              Student of Backend Web Development
+            </h2>
+          </div>
+        </div>
     </div>
   </section>
 
