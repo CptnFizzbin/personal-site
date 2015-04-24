309f9e70328412329dc65aeb19cdb0729fa913bb
Stephen <code@cptnfizzbin.ca>
2015-04-23 20:21:01 -0400

    update footer

--- app/public/layouts/_footer.php
+++ app/public/layouts/_footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="row">
     <div class="small-12 columns">
-      <small>&copy; Copyright 2014 Stephen A. Wilson</small>
+      <small>&copy; Copyright 2015 Stephen A. Wilson -- Git: 9e1a4c311503ad24e0c32bc6bc258dad80437b04</small>
     </div>
   </div>
 </footer>
