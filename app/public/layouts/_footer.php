de8d8afb3d3b8d62a707a8223f991ef621ba7a68
Stephen <code@cptnfizzbin.ca>
2015-07-21 19:18:59 -0400

    Move everything into folder

--- app/public/layouts/_footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-  <div class="row">
-    <div class="small-12 columns">
-      <small>&copy; Copyright 2015 Stephen A. Wilson | Last updated: April 23, 2015 | Git: 33321fd09fb74102d4711ba6a58280d3841d4057</small>
-    </div>
-  </div>
-</footer>
