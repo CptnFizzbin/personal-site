ae3db406750754902ee0ac13cfb5f8fc40f41fbd
Stephen <code@cptnfizzbin.ca>
2015-08-05 15:15:30 -0400

    Consolidate resume page into home page

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -15,11 +15,6 @@
         templateUrl: 'app/pages/main/main.html',
         controller: 'MainCtrl'
       })
-      .state('resume', {
-        url: '/resume',
-        templateUrl: 'app/pages/resume/resume.html',
-        controller: 'ResumeCtrl'
-      })
       .state('projects', {
         url: '/projects',
         templateUrl: 'app/pages/projects/projects.html',
