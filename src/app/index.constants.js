32874b40d1f86e00df39171b6c4f51a299f543f7
Stephen <code@cptnfizzbin.ca>
2015-08-27 13:11:05 -0400

    remove toastr and moment

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,10 +1,7 @@
-/* global malarkey:false, toastr:false, moment:false */
 (function() {
   'use strict';
 
   angular
-    .module('portfolio')
-    .constant('toastr', toastr)
-    .constant('moment', moment);
+    .module('portfolio');
 
 })();
