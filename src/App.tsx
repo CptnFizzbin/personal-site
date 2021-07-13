81138a79cb729e34f7c0d66bde892491d566fab4
Stephen <code@cptnfizzbin.ca>
2021-07-13 19:45:12 -0400

    add basic header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react'
 
-function App() {
+import { Header } from './Header/Header'
+
+import styles from './App.module.scss'
+
+const App: FC = () => {
+  const scrolledToTop = true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Header expanded={scrolledToTop} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
