fb7ec8b2ff439e9381952a7cbd68d8330970450f
Stephen <code@cptnfizzbin.ca>
2021-07-20 17:34:46 -0400

    add headers for sections

--- src/Sections/Education.tsx
+++ src/Sections/Education.tsx
@@ -2,11 +2,14 @@ import { FC } from 'react'
 import { SectionProps } from './SectionProps'
 
 import styles from './Education.module.scss'
+import { Header } from '../UI/Header'
 
 export const Education: FC<SectionProps> = ({
   gridArea,
 }) => {
   return (
-    <div className={styles.Education} style={{ gridArea }}>Education</div>
+    <div className={styles.Education} style={{ gridArea }}>
+      <Header title="Education" />
+    </div>
   )
 }
--- src/Sections/Projects.tsx
+++ src/Sections/Projects.tsx
@@ -2,11 +2,14 @@ import { FC } from 'react'
 import { SectionProps } from './SectionProps'
 
 import styles from './Projects.module.scss'
+import { Header } from '../UI/Header'
 
 export const Projects: FC<SectionProps> = ({
   gridArea,
 }) => {
   return (
-    <div className={styles.Projects} style={{ gridArea }}>Projects</div>
+    <div className={styles.Projects} style={{ gridArea }}>
+      <Header title="Projects" />
+    </div>
   )
 }
