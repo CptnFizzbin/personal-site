83d6aec388ffaa57a30eada5baaa38e8699dc7b0
Stephen <code@cptnfizzbin.ca>
2021-07-30 22:07:05 -0400

    fade floating header out when scrolled

--- src/Page/PageHeader.tsx
+++ src/Page/PageHeader.tsx
@@ -1,4 +1,6 @@
-import { FC } from 'react'
+import useWindowScroll from '@react-hook/window-scroll'
+import classNames from 'classnames'
+import { FC, useMemo } from 'react'
 
 import { NamePlate } from './NamePlate'
 import { ProfilePicture } from './ProfilePicture'
@@ -6,10 +8,20 @@ import { ProfilePicture } from './ProfilePicture'
 import styles from './PageHeader.module.scss'
 
 export const PageHeader: FC = () => {
-  return (
-    <div className={styles.PageHeader}>
-      <ProfilePicture width={100} />
-      <NamePlate />
-    </div>
-  )
+  const scrolled = useWindowScroll() > 200
+
+  return useMemo(() => {
+    console.log(scrolled)
+    return (
+      <div
+        className={classNames(
+          styles.PageHeader,
+          { [styles.scrolled]: scrolled },
+        )}
+      >
+        <ProfilePicture width={100} />
+        <NamePlate />
+      </div>
+    )
+  }, [scrolled])
 }
