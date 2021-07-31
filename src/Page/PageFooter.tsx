f9fcffb3ceed6237c41e3345a7d3ad1e98b23325
Stephen <code@cptnfizzbin.ca>
2021-07-30 22:30:52 -0400

    add contact info to footer

--- src/Page/PageFooter.tsx
+++ src/Page/PageFooter.tsx
@@ -5,7 +5,12 @@ import styles from './PageFooter.module.scss'
 export const PageFooter: FC = () => {
   return (
     <div className={styles.PageFooter}>
-
+      <span className={styles.FooterLeft}>
+        <a href={'https://github.com/zeragamba'} target="_blank" rel="noreferrer">GitHub</a>
+      </span>
+      <span className={styles.FooterRight}>
+        <a href={'mailto:contact@stephenawilson.ca'}>contact@stephenawilson.ca</a>
+      </span>
     </div>
   )
 }
