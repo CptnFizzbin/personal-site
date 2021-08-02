628f67953246dabacd0e399a02e645cdbf51bab0
Stephen <code@cptnfizzbin.ca>
2021-08-02 18:01:29 -0400

    add printable Resume

--- src/Page/PageFooter.tsx
+++ src/Page/PageFooter.tsx
@@ -8,6 +8,9 @@ export const PageFooter: FC = () => {
       <span className={styles.FooterLeft}>
         <a href={'https://github.com/zeragamba'} target="_blank" rel="noreferrer">GitHub</a>
       </span>
+      <span className={styles.FooterCenter}>
+        <a href={'/resume.pdf'} target="_blank" rel="noreferrer">Printable</a>
+      </span>
       <span className={styles.FooterRight}>
         <a href={'mailto:contact@stephenawilson.ca'}>contact@stephenawilson.ca</a>
       </span>
